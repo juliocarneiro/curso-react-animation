775a7e7d5ad98dabdf42c23d2d78a6407e4d4bf7
juliocarneiro <juliocarneiro@gmail.com>
2018-07-11 22:11:48 -0300

    apresentando e instalando lib css

---
+++ 1/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <p className="animated infinite bounce">Teste do curso animações com react e css3</p>
+      </div>
+    );
+  }
+}
+
+export default App;
